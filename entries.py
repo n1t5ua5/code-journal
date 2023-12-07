2c07bed81d8124f63a1caf9d3e1c7eeb2da70167
n1tsua5 <110934299+nitsua5@users.noreply.github.com>
2023-12-07 12:13:38 -0500

    Update entries.py
    12/7/23 entry complete

--- entries.py
+++ entries.py
@@ -1,12 +1,23 @@
 # FALL / WINTER 2023
 
 # 12/06/2023
-# Today I picked up where I left off & started working on this 30 day JavaScript challenge I was told to practice with again & it's made a big difference so far. I familiarized myself with methods like .strokeStyle & .moveTo, grew accustom to using tools like console.table & console.group instead of just console.log while also getting to know how to use GitHub better. I'm more familiar with GitLab, so I know a lot of Git commands already but it's still new terrain.
-# After many attempts of failing to push to the proper branch, I stepped back to think & realized that I forgot to merge my old branch with the new before inputting add ., commit -m "your-text-here" & git push origin main into my terminal. I also discovered a few other standard commands like git config user.name & user.email. Now all my work is published.
+# Today I picked up where I left off & started working on this 30 day JavaScript challenge I was told to practice with again & it's made a big difference so far.
+# I familiarized myself with methods like .strokeStyle & .moveTo, grew accustom to using tools like console.table & console.group instead of just console.log
+# while also getting to know how to use GitHub better. I'm more familiar with GitLab, so I know a lot of Git commands already but it's still new terrain.
+# After many attempts of failing to push to the proper branch, I stepped back to think & realized that I forgot to merge my old branch with the new before
+# inputting add ., commit -m "your-text-here" & git push origin main into my terminal. I also discovered a few other standard commands like git config
+# user.name & user.email. Now all my work is published.
 
 # 12/07/2023
-# Documentating everything I'm learning like this helping me a lot, I'm really happy I finally started this. After a few more additons I'll make a basic web application for this whole project.
-# Working again with the JavaScript30 Challenge, always to find that there really is something new to learn everyday. I began making a function called handleCheck which finds if a user has clicked on a checkbox or not. I'm also installing GitHub Co-Pilot today since I am learning all of these commands on my own for the first time, I need some assistance.
+# Documentating everything I'm learning like this helping me a lot, I'm really happy I finally started this. After a few more additons I'll make a basic web
+# application for this whole project. Working again with the JavaScript30 Challenge, always to find that there really is something new to learn everyday.
+# I began making a function called handleCheck which finds if a user has clicked on a checkbox or not. I'm also installing GitHub Co-Pilot today since
+# I am learning all of these commands on my own for the first time, I need some assistance. After installing it, I thought it was more geared towards
+# making commands in the terminal instead of vscode but It's working I suppose. I'm also starting to fully realize the depth of GitHub as I
+# start to work in other branches now. The handleCheck function works now, when you select a checkbox on the form & then click shift, after if you click
+# on another item in the form, you will have selected every option in between. That's why I declared variable named inBetween to clarify which items
+# must be selected from the if statement in the handleCheck function. You can find the last commit I made for that project here below
+# https://github.com/nitsua5/js30practice/commits/master
 
 # 12/08/2023
 # ?

d23f05595992a31e60ebffdf8d80555610e3e41b
n1tsua5 <110934299+nitsua5@users.noreply.github.com>
2023-12-20 12:14:51 -0500

    Update entries.py

--- entries.py
+++ entries.py
@@ -1,7 +1,19 @@
 # DECEMBER '23
 
-# 12/20/2023
+# 12/21/2023
 # ???
+
+# 12/20/2023
+# Trying to tally strings with the .reduce method this morning. There is a list of video's time-length as strings & I am going to
+# find the total length of all videos as a number. I've used .reduce before too, so it'll be good to utilize it again & learn
+# something new in the process. First I declared a variabled named timeNodes & set it equal to Array.from(document.querySelectorAll(
+# '[data-time]')); which selects all the DOM elements that have a data-time attribute. Then I declared a new variable 'seconds'
+# to retrieve the seconds for each time listed by using the .map & .reduce function to extract all of the data-time values & store
+# the data in a new array containing the values. I revisited using the % operator to return whatever is left over from the seconds
+# variable to determine how many seconds are left from the original <ul> elements with a class of "videos". Everything works, then
+# I calculated the total number of minutes by declaring a variable named mins & setting it equal to Math.floor(secondsLeft / 60);
+# which is basically the same thing I did for secondsLeft.
+# REPO: github.com/nitsua5/js30practice/commits/master
 
 # 12/19/2023
 # Introduced to the .replace method today when making a function called sortedBands which should display a list of bands in array
